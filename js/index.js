5447c03930f4600a8f04403c506df67c065f1a3d
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-07-27 10:00:07 +0700

    progess index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import { checkD } from "./util.js";
+
 export default function renderIndex(data) {
     console.log(data)
     let mainContent = '';
@@ -9,21 +11,54 @@ export default function renderIndex(data) {
         </div>
     `;
 
+    // Info
+    mainContent += `
+        <div class="container">
+            <div class="row">
+                <h3 class="center-align">${data.competition.name}</h3>
+            </div>
+            <div class="row">
+                <div class="col s12 m10 l8 offset-m1 offset-l2">
+                    <table class="w-100 centered striped highlight">
+                        <tbody>
+                            <tr>
+                                <td>Name</td>
+                                <td>${data.competition.name}</td>
+                            </tr>
+                            <tr>
+                                <td>Name</td>
+                                <td>${data.competition.code}</td>
+                            </tr>
+                            <tr>
+                                <td>Event Start</td>
+                                <td>${data.season.startDate}</td>
+                            </tr>
+                            <tr>
+                                <td>Event End</td>
+                                <td>${data.season.endDate}</td>
+                            </tr>
+                            <tr>
+                                <td>Winner</td>
+                                <td>${checkD(data.season.winner)}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    `
+
     $('#content').html(mainContent);
     
     // JS DOM
-
     const WWidth = $(window).width();
     if(WWidth <= 600) {
         $('#league-parallax').attr("src", "assets/banner.jpg");
-        console.log('sm')
     }
     if(WWidth >= 601 && WWidth <= 992) {
-        console.log('md')
         $('#league-parallax').attr("src", "assets/bannermd.jpg");
     }
     if(WWidth > 992) {
-        console.log('lg')
         $('#league-parallax').attr("src", "assets/bannerlg.jpg");
     }
     
