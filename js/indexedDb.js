abc0c932c8cfa5b4399ec7e4eeb9153074623b49
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-07-28 13:41:47 +0700

    idb on progress

---
+++ js/indexedDb.js
@@ -0,0 +1,41 @@
+const openDb = idb.open('saved-team', 1, upgDb => {
+    upgDb.createObjectStore("teams", {keyPath: "id"}).createIndex("id_team", "id_team", {unique:true});
+});
+
+function saveToDb(team) {
+    openDb.then(db=> {
+        const trx = db.transaction("teams","readwrite");
+        trx.objectStore("teams").add(team)
+        return trx.complete;
+    }).then(()=> {
+        M.toast({html: `${team.shortName} saved`})
+    });
+}
+
+function getTeam(id) {
+    return new Promise(r=>{
+        openDb.then(db=> db.transaction("teams", "readonly").objectStore("teams").get(id))
+            .then(team=>{
+                r(team)
+            });
+    });
+}
+
+function getTeams() {
+    return new Promise(r=>{
+        openDb.then(db=> db.transaction("teams", "readonly").objectStore("teams").getAll())
+            .then(teams => {
+                r(teams)
+            })
+    })
+}
+
+function deleteTeam(team) {
+    openDb.then(db=>{
+        const trx = db.transaction("teams", "readwrite");
+        trx.objectStore("teams").delete(team.id);
+        return trx.complete;
+    }).then(() => {
+        M.toast({html: `${team.shortName} deleted`})
+    })
+}
