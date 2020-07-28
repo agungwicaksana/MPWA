3d4a99cb3f050c74c3e70cc32052efd8412f7892
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-07-28 16:17:00 +0700

    progress FCM

--- regServiceWorker.js
+++ regServiceWorker.js
@@ -1,7 +1,42 @@
 if("serviceWorker" in navigator) {
     navigator.serviceWorker.register('/serviceWorker.js')
         .then(() => console.log("Service Worker berhasil didaftarkan"))
-        .catch(() => console.log("Service Worker gagal didaftarkan"))
+        .catch(() => console.log("Service Worker gagal didaftarkan"));
+    
+    if("Notification" in window) {
+        Notification.requestPermission().then(res => {
+            if(res==="granted"){
+                if("PushManager" in window){
+                    navigator.serviceWorker.getRegistration().then(reg=>{
+                        reg.pushManager.subscribe({
+                            userVisibleOnly: true,
+                            applicationServerKey: convertToUint("")
+                        }).then(subs=>{
+                            console.log('Berhasil subscribe')
+                            console.log('endpoint: ',endpoint)
+                            console.log('p256dh: ', btoa(String.fromCharCode.apply(null, new Uint8Array(subs.getKey("p256dh")))))
+                            console.log('Auth: ', btoa(String.fromCharCode.apply(null, new Uint8Array(subs.getKey("auth")))))
+                        }).catch(err=>{
+                            console.log('Subscribe gagal: ', err)
+                        })
+                    })
+                }
+            }
+        })
+    }
 } else {
     alert('Browser anda tidak dapat menjalan PWA. Ganti dengan browser lain!')
+}
+
+function convertToUint(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 }
