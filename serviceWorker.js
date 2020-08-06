749ca03994e32777d95bc2bde86f70e459abfb96
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-08-06 08:31:59 +0700

    change sw to workbox

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,71 +1,65 @@
-const cacheName = "uefa-app-v1";
-const cachedUrls = [
-    "/",
-    "/index.html",
-    "/favorite.html",
-    "/match.html",
-    "/team.html",
-    "/team-detail.html",
-    "/saved-team.html",
-    "/manifest.json",
-    "/regServiceWorker.js",
-    "/serviceWorker.js",
-    "/assets/32.png",
-    "/assets/144.png",
-    "/assets/192.png",
-    "/assets/512.png",
-    "/assets/Ball.svg",
-    "/assets/banner.jpg",
-    "/assets/bannerlg.jpg",
-    "/assets/bannermd.jpg",
-    "/assets/navLogo.svg",
-    "/assets/sq_logo.svg",
-    "/css/materialize.min.css",
-    "/css/style.css",
-    "/css/util.css",
-    "/js/apiFetch.js",
-    "/js/customE.js",
-    "/js/favorite.js",
-    "/js/idb.js",
-    "/js/index.js",
-    "/js/indexedDb.js",
-    "/js/match.js",
-    "/js/materialize.min.js",
-    "/js/static.js",
-    "/js/team.js",
-    "/js/team-detail.js",
-    "/js/util.js",
-    "https://fonts.googleapis.com/icon?family=Material+Icons",
-    "https://code.jquery.com/jquery-3.5.1.min.js"
-];
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-self.addEventListener("install", event => {
-    event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(cachedUrls)))
-});
+workbox.precaching.precacheAndRoute(
+    [
+        {url: "/index.html", revision: "1"},
+        {url: "/favorite.html", revision: "1"},
+        {url: "/match.html", revision: "1"},
+        {url: "/team.html", revision: "1"},
+        {url: "/team-detail.html", revision: "1"},
+        {url: "/saved-team.html", revision: "1"},
+        {url: "/manifest.json", revision: "1"},
+        {url: "/regServiceWorker.js", revision: "1"},
+        {url: "/serviceWorker.js", revision: "1"},
+        {url: "/assets/144.png", revision: "1"},
+        {url: "/assets/192.png", revision: "1"},
+        {url: "/assets/512.png", revision: "1"},
+        {url: "/assets/Ball.svg", revision: "1"},
+        {url: "/assets/banner.jpg", revision: "1"},
+        {url: "/assets/bannerlg.jpg", revision: "1"},
+        {url: "/assets/bannermd.jpg", revision: "1"},
+        {url: "/assets/navLogo.svg", revision: "1"},
+        {url: "/assets/sq_logo.svg", revision: "1"},
+        {url: "/css/materialize.min.css", revision: "1"},
+        {url: "/css/style.css", revision: "1"},
+        {url: "/css/util.css", revision: "1"},
+        {url: "js/apiFetch.js", revision: "1"},
+        {url: "js/customE.js", revision: "1"},
+        {url: "js/favorite.js", revision: "1"},
+        {url: "js/idb.js", revision: "1"},
+        {url: "js/index.js", revision: "1"},
+        {url: "js/indexedDb.js", revision: "1"},
+        {url: "js/match.js", revision: "1"},
+        {url: "js/materialize.min.js", revision: "1"},
+        {url: "js/static.js", revision: "1"},
+        {url: "js/team.js", revision: "1"},
+        {url: "js/team-detail.js", revision: "1"},
+        {url: "js/util.js", revision: "1"},
+        {url: "https://fonts.googleapis.com/icon?family=Material+Icons", revision: "1"},
+        {url: "https://code.jquery.com/jquery-3.5.1.min.js", revision: "1"},
+        {url: "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js", revision: "1"}
+    ]
+)
 
-self.addEventListener("fetch", event => {
-    const apiUrl = "https://api.football-data.org/v2";
-    if(event.request.url.indexOf(apiUrl)>-1) {
-        event.respondWith(caches.open(cacheName).then(async (cache) => {
-            return await fetch(event.request).then(resp => {
-                cache.put(event.request.url, resp.clone());
-                return resp;
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'img',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 20
             })
-        }))
-    } else {
-        event.respondWith(caches.match(event.request, {ignoreSearch: true}).then(resp => resp || fetch(event.request)))
-    }
-})
+        ]
+    })
+)
 
-self.addEventListener("activate", event => {
-    event.waitUntil(caches.keys().then(cNames => Promise.all(
-        cNames.map(cName => {
-            if(cName !== cName && cName.startsWith("uefa-app")) {
-                return caches.delete(cName)
-            }
-        })
-    )))
-})
+workbox.routing.registerRoute(
+    /^https:\/\/api\.football-data\.org\/v2/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: "api-data"
+    })
+)
 
 self.addEventListener("push", event =>{
     let body;
